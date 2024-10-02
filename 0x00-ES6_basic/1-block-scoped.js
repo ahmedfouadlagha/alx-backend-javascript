4c25497338d21866bf35afdff7bba9583a64f4a5
Ahmed Fouad LAGHA <laghaahmedfouad@gmail.com>
2024-10-02 14:00:56 +0100

    update file

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;  // Utilisation de const car task ne changera pas
-  const task2 = true;  // Utilisation de const car task2 ne changera pas
+  const task = false;
+  const task2 = true;
 
+  // eslint-disable-next-line no-empty
   if (trueOrFalse) {
-    let task = true;   // Nouvelle déclaration limitée à ce bloc
-    let task2 = false; // Nouvelle déclaration limitée à ce bloc
   }
 
-  return [task, task2];  // Renvoie les valeurs en dehors du bloc if
+  return [task, task2];
 }
